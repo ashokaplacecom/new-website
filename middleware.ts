6efd8cb47da84b4d1622299a06fb1459e1125938
Soham Tulsyan <soham.tulsyan_ug2023@ashoka.edu.in>
2026-03-25 17:30:06 +0530

    Better authentication for API while accessing through chrome extension

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,18 @@ import { NextRequest, NextResponse } from 'next/server'
 export function middleware(req: NextRequest) {
     const apiKey = req.headers.get('x-api-key')
 
-    const VALID_API_KEYS = new Set([
-        process.env.API_KEY_WEB_EXTENSION,
-        process.env.API_KEY_FRONTEND,
-    ])
+    const isWebExtension = apiKey === process.env.API_KEY_WEB_EXTENSION;
+    const isFrontend = apiKey === process.env.API_KEY_FRONTEND;
 
-    if (!apiKey || !VALID_API_KEYS.has(apiKey)) {
+    if (!apiKey || (!isWebExtension && !isFrontend)) {
         return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+    }
+
+    if (isWebExtension) {
+        const origin = req.headers.get('origin')
+        if (origin !== process.env.ALLOWED_EXTENSION_ORIGIN) {
+            return NextResponse.json({ error: 'Unauthorized Origin' }, { status: 401 })
+        }
     }
 
     return NextResponse.next()
